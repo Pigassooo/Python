7f0762aacecc6798f165c406cf05987d4fded5e9
“Pigassooo” <1219632665@qq.com>
2024-04-10 23:22:00 +0800

    Django02

--- Django_project/blog/views.py
+++ Django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts=[
+    {
+        'author': 'Pigasso',
+        'title':'Blog Post 1',
+        'content':'This is the first post',
+        'date_posted':'April 9, 2024',
+    },
+    {
+        'author': 'Ivy',
+        'title':'Blog Post 2',
+        'content':'This is the second post',
+        'date_posted':'April 10, 2024',
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html',{'title':'About'})
